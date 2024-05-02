3fbb7ee41b650ea1bd8cd7c37e6fd591636c92f6
harshit123vyas <167099626+harshit123vyas@users.noreply.github.com>
2024-05-02 22:06:41 +0530

    Update TaskService.java

--- Todo_Service/src/main/java/com/tasktracker/todolist/service/TaskService.java
+++ Todo_Service/src/main/java/com/tasktracker/todolist/service/TaskService.java
@@ -16,25 +16,25 @@ public interface TaskService {
 
 	public TaskResponse<Task> getByUserId(Long userId);
 
-	public Task addTask(Task task);
+	public TaskResponse<Task> addTask(Task task);
 
-	public List<Task> getAllTask(Pageable paging);
+	public TaskResponse<List<Task>> getAllTask(Pageable paging);
 
-	public Task getTaskById(Integer id);
+	public TaskResponse<Task> getTaskById(Integer id);
 
-	public List<Task> getTaskByTitle(String title);
+	public TaskResponse<List<Task>> getTaskByTitle(String title);
 
-	public List<Task> getTaskByCompletionDate(LocalDateTime completionDate);
+	public TaskResponse<List<Task>> getTaskByCompletionDate(LocalDateTime completionDate);
 
-	public List<Task> getTaskByCreationDate(LocalDateTime creationDate);
+	public TaskResponse<List<Task>> getTaskByCreationDate(LocalDateTime creationDate);
 
-	public List<Task> getAllRemainingTask();
+	public TaskResponse<List<Task>> getAllRemainingTask();
 
-	public List<Task> getIncompleteTask();
+	public TaskResponse<List<Task>> getIncompleteTask();
 
 	public boolean deleteTaskById(Integer id);
 
-	public Task updateTask(Task task, Integer id);
+	public TaskResponse<Task> updateTask(Task task, Integer id);
 
 	public String ScheduleNotification();
 
